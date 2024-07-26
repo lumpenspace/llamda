11f37677f7649f7395750f124cff4b3a899a0a49
lumpenspace <125578322+lumpenspace@users.noreply.github.com>
2024-07-26 14:46:20 -0700

    Extract Llamda functions

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,11 @@
 Llamda is a Python library for transforming Python functions into LLM function calls.
 """
 
-from .llamda import Llamda
+from .llamda_function.llamda_functions import LlamdaFunctions
 from .llamda_function import __all__ as llamda_fn
 
 
 __all__: list[str] = [
-    "Llamda",
+    "LlamdaFunctions",
     "llamda_fn",
 ]
--- src/llamda_function/__init__.py
+++ src/llamda_function/__init__.py
@@ -1,4 +1,5 @@
 from .llamda_function import LlamdaFunction
+from .llamda_functions import LlamdaFunctions
 from .process_fields import process_fields
 
-__all__ = ["LlamdaFunction", "process_fields"]
+__all__ = ["LlamdaFunction", "process_fields", "LlamdaFunctions"]
